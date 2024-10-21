bc28279a9054eca88f0e0ca8b255100c671e3152
andrmuratov <79189791237@yandex.ru>
2024-10-22 01:11:01 +0300

    Update scripts.js

--- LAB7/scripts.js
+++ LAB7/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $('.slider').slick({
-        dots: true,                // Показ точек навигации
-        infinite: true,            // Бесконечная прокрутка
-        slidesToShow: 3,           // Показывать 3 изображения на экране
-        slidesToScroll: 1,         // Прокручивать по 1 изображению
+        dots: true,                 
+        infinite: true,         
+        slidesToShow: 3,         
+        slidesToScroll: 1,        
         prevArrow: '<button type="button" class="slick-prev">←</button>',
         nextArrow: '<button type="button" class="slick-next">→</button>',
         responsive: [
             {
-                breakpoint: 768,    // Экран шириной до 768px
+                breakpoint: 768,   
                 settings: {
-                    slidesToShow: 1, // Показывать 1 изображение на экране
+                    slidesToShow: 1,  
                     slidesToScroll: 1
                 }
             }
