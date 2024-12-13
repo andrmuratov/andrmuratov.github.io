0041c964e3b1455a66090a927d99672a752ef9a5
andrmuratov <79189791237@yandex.ru>
2024-12-13 18:32:13 +0300

    Update project.js

--- project/project.js
+++ project/project.js
@@ -1,9 +1,11 @@
 window.onload = function () {
   let start = false;
+  
   function slicker() {
     let vw = window.innerWidth;
     let vh = window.innerHeight;
     console.log(vh, vw);
+    
     if (start) {
       $(".autoplay").slick("unslick");
     }
@@ -13,20 +15,20 @@ window.onload = function () {
         arrows: false,
         dots: true,
         infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        slidesToScroll: 1,
+        slidesToShow: 5
       });
       setTimeout(function () {
         $(".autoplay2").slick({
           arrows: false,
           dots: true,
           infinite: true,
-          slidesToShow: 5,
-          slidesToScroll: 1,
           autoplay: true,
           autoplaySpeed: 2000,
+          slidesToScroll: 1,
+          slidesToShow: 5
         });
       }, 800);
     } else if (vw >= 600) {
@@ -34,20 +36,20 @@ window.onload = function () {
         arrows: false,
         dots: true,
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        slidesToScroll: 1,
+        slidesToShow: 3
       });
       setTimeout(function () {
         $(".autoplay2").slick({
           arrows: false,
           dots: true,
           infinite: true,
-          slidesToShow: 3,
-          slidesToScroll: 1,
           autoplay: true,
           autoplaySpeed: 2000,
+          slidesToScroll: 1,
+          slidesToShow: 3
         });
       }, 800);
     } else if (vw <= 480) {
@@ -55,24 +57,25 @@ window.onload = function () {
         arrows: false,
         dots: true,
         infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        slidesToScroll: 1,
+        slidesToShow: 2
       });
       setTimeout(function () {
         $(".autoplay2").slick({
           arrows: false,
           dots: true,
           infinite: true,
-          slidesToShow: 2,
-          slidesToScroll: 1,
           autoplay: true,
           autoplaySpeed: 2000,
+          slidesToScroll: 1,
+          slidesToShow: 2
         });
       }, 800);
     }
   }
+  
   slicker();
   start = true;
 
@@ -86,6 +89,7 @@ $(".mob_menu").on("click", function () {
 });
 
 $(".a").css("height", $(".aa > div:eq(0)").height());
+
 function aa(p) {
   console.log(p);
   $(".aa > div").css("opacity", "0");
@@ -110,21 +114,17 @@ function aa(p) {
   $(".ednum").html((p + 1).toString().padStart(2, "0"));
 }
 
-(p = 0), (pl = $(".aa > div").length - 1);
+let p = 0;
+let pl = $(".aa > div").length - 1;
+
 $(".b2").on("click", function () {
   if (p == 0) p = pl;
   else p--;
   aa(p);
 });
+
 $(".b1").on("click", function () {
   if (p == pl) p = 0;
   else p++;
   aa(p);
-});
-
-/*Footer*/
-
-document.addEventListener("DOMContentLoaded", (event) => {
-  const recaptcha = document.querySelector(".g-recaptcha");
-  recaptcha.setAttribute("data-theme", "dark");
 });
